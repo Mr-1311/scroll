0bc9f8b13cad619769238febe8bd9783000d87d0
metin ur <metinur.1311@gmail.com>
2020-01-05 02:14:18 +0300

    Emphasis and Monospace

--- sop/src/lib.rs
+++ sop/src/lib.rs
@@ -1,13 +1,49 @@
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref REGEX_ALL: Regex = Regex::new(
+        r"(?m)(?P<headline>^\*+ .*\n?)|(?P<list>^[ \t]*(?:-|\+|[ \t]+\*|\d+\.|\d+\)) .*\n?)|(?P<keyword>^[ \t]*#\+.*:.*\n)|(?P<table>^ *\|.*\n)"
+    ).unwrap();
+    static ref REGEX_TEXT: Regex = Regex::new(
+        r"(?m)(?P<bold>\*\w+(?:\s+\w+)*\*)|(?P<italic>/\w+(?:\s+\w+)*/)|(?P<code>~\w+(?:\s+\w+)*~)|(?P<underline>_\w+(?:\s+\w+)*_)|(?P<strike>\+\w+(?:\s+\w+)*\+)|(?P<link>\[\[.+\]\])"
+    ).unwrap();
+}
+
 #[derive(Debug)]
 enum OrgElement {
     Section(Vec<OrgElement>),
-    Paragraph { childs: Vec<OrgElement> },
-    Headline { level: u8, title: String },
-    Keyword { key: String, value: String },
+    Paragraph {
+        childs: Vec<OrgElement>,
+    },
+    Headline {
+        level: u8,
+        title: Vec<OrgElement>,
+    },
+    Keyword {
+        key: String,
+        value: String,
+    },
+    Link {
+        link_type: LinkType,
+        link: String,
+        desc: Vec<OrgElement>,
+    },
     Text(String),
     Bold(String),
+    Italic(String),
+    Underline(String),
+    Code(String),
+    StrikeThrough(String),
 }
 
+#[derive(Debug)]
+enum LinkType {
+    URL,
+    FILE,
+}
 fn create_headline(raw_value: &str) -> OrgElement {
     let mut level: u8 = 0;
     for c in raw_value.chars() {
@@ -17,12 +53,13 @@ fn create_headline(raw_value: &str) -> OrgElement {
         }
         break;
     }
-    let mut title = String::from("");
+    let mut title = Vec::new();
     if let Some(t) = raw_value.get((level + 1) as usize..) {
-        title = t.trim().to_string();
+        title = handle_text(t.trim().to_owned());
     }
     OrgElement::Headline { level, title }
 }
+
 fn create_keyword(raw_value: &str) -> OrgElement {
     let mut key = String::new();
     let mut val = String::new();
@@ -44,6 +81,82 @@ fn create_keyword(raw_value: &str) -> OrgElement {
         value: val.trim().to_string(),
     }
 }
+fn create_paragraph(raw_value: String) -> OrgElement {
+    OrgElement::Paragraph {
+        childs: handle_text(raw_value),
+    }
+}
+
+fn handle_text(raw_value: String) -> Vec<OrgElement> {
+    let mut texts: Vec<OrgElement> = Vec::new();
+    let mut cur_index = 0;
+    for cap in REGEX_TEXT.captures_iter(&raw_value) {
+        if let Some(c) = cap.name("bold") {
+            texts.push(OrgElement::Text(
+                raw_value.get(cur_index..c.start()).unwrap().to_owned(),
+            ));
+            texts.push(OrgElement::Bold(
+                c.as_str().get(1..c.as_str().len() - 1).unwrap().to_owned(),
+            ));
+            cur_index = c.end();
+        }
+        if let Some(c) = cap.name("italic") {
+            texts.push(OrgElement::Text(
+                raw_value.get(cur_index..c.start()).unwrap().to_owned(),
+            ));
+            texts.push(OrgElement::Italic(
+                c.as_str().get(1..c.as_str().len() - 1).unwrap().to_owned(),
+            ));
+            cur_index = c.end();
+        }
+        if let Some(c) = cap.name("code") {
+            texts.push(OrgElement::Text(
+                raw_value.get(cur_index..c.start()).unwrap().to_owned(),
+            ));
+            texts.push(OrgElement::Code(
+                c.as_str().get(1..c.as_str().len() - 1).unwrap().to_owned(),
+            ));
+            cur_index = c.end();
+        }
+        if let Some(c) = cap.name("underline") {
+            texts.push(OrgElement::Text(
+                raw_value.get(cur_index..c.start()).unwrap().to_owned(),
+            ));
+            texts.push(OrgElement::Underline(
+                c.as_str().get(1..c.as_str().len() - 1).unwrap().to_owned(),
+            ));
+            cur_index = c.end();
+        }
+        if let Some(c) = cap.name("strike") {
+            texts.push(OrgElement::Text(
+                raw_value.get(cur_index..c.start()).unwrap().to_owned(),
+            ));
+            texts.push(OrgElement::StrikeThrough(
+                c.as_str().get(1..c.as_str().len() - 1).unwrap().to_owned(),
+            ));
+            cur_index = c.end();
+        }
+    }
+    texts.push(OrgElement::Text(
+        raw_value.get(cur_index..).unwrap().to_owned(),
+    ));
+    texts
+}
+fn generate_html_for_text(t: &Vec<OrgElement>) -> String {
+    let mut out = String::new();
+    for e in t {
+        match e {
+            OrgElement::Text(s) => out.push_str(&format!("{}", s)),
+            OrgElement::Bold(s) => out.push_str(&format!("<b>{}</b>", s)),
+            OrgElement::Italic(s) => out.push_str(&format!("<i>{}</i>", s)),
+            OrgElement::Underline(s) => out.push_str(&format!("<u>{}</u>", s)),
+            OrgElement::StrikeThrough(s) => out.push_str(&format!("<s>{}</s>", s)),
+            OrgElement::Code(s) => out.push_str(&format!("<code>{}</code>", s)),
+            _ => (),
+        }
+    }
+    out
+}
 
 #[derive(Debug)]
 pub struct OrgAST {
@@ -62,29 +175,25 @@ impl OrgAST {
         }
     }
     fn handle_undetect_str(&mut self, start: usize, end: usize, raw_str: &str) {
-        let mut cur_parag_str = String::new();
+        let mut cur_parag = String::new();
         if let Some(s) = raw_str.get(self.last_element_index..start) {
             for line in s.lines() {
-                if line == "" && !cur_parag_str.is_empty() {
-                    cur_parag_str.pop();
+                if line == "" && !cur_parag.is_empty() {
+                    cur_parag.pop();
 
-                    self.add_child(OrgElement::Paragraph {
-                        childs: vec![OrgElement::Text(cur_parag_str.clone())],
-                    });
-                    cur_parag_str.clear();
+                    self.add_child(create_paragraph(cur_parag.clone()));
+                    cur_parag.clear();
                     continue;
                 }
                 if line != "" {
-                    cur_parag_str.push_str(line);
-                    cur_parag_str.push(' ');
+                    cur_parag.push_str(line);
+                    cur_parag.push(' ');
                 }
             }
         }
-        if !cur_parag_str.is_empty() {
-            cur_parag_str.pop();
-            self.add_child(OrgElement::Paragraph {
-                childs: vec![OrgElement::Text(cur_parag_str.clone())],
-            });
+        if !cur_parag.is_empty() {
+            cur_parag.pop();
+            self.add_child(create_paragraph(cur_parag.clone()));
         }
         self.last_element_index = end;
     }
@@ -141,16 +250,9 @@ impl OrgParser {
         )
     }
     pub fn create_ast(&self) -> OrgAST {
-        use regex::Regex;
-
-        let re = Regex::new(
-            r"(?m)(?P<headline>^\*+ .*\n?)|(?P<list>^[ \t]*(?:-|\+|[ \t]+\*|\d+\.|\d+\)) .*\n?)|(?P<keyword>^[ \t]*#\+.*:.*\n)|(?P<table>^ *\|.*\n)",
-        )
-        .unwrap();
-
         let mut doc = OrgAST::new();
 
-        for cap in re.captures_iter(&self.raw_str) {
+        for cap in REGEX_ALL.captures_iter(&self.raw_str) {
             if let Some(c) = cap.name("headline") {
                 doc.handle_undetect_str(c.start(), c.end(), &self.raw_str);
                 doc.add_child(create_headline(c.as_str()));
@@ -180,6 +282,7 @@ impl OrgParser {
         }
 
         doc.handle_undetect_str(self.raw_str.len(), self.raw_str.len(), &self.raw_str);
+        println!("{:#?}", doc);
         doc
     }
     pub fn create_html(&self) -> String {
@@ -193,16 +296,14 @@ impl OrgParser {
             for el in v {
                 match el {
                     OrgElement::Headline { level, title } => {
-                        out_html.push_str(&format!("<h{l}>{}</h{l}>\n", title, l = level));
+                        out_html.push_str(&format!(
+                            "<h{l}>{}</h{l}>\n",
+                            generate_html_for_text(title),
+                            l = if level > &6 { &6u8 } else { level }
+                        ));
                     }
                     OrgElement::Paragraph { childs } => {
-                        let mut paragraph = String::new();
-                        for c in childs {
-                            if let OrgElement::Text(s) = c {
-                                paragraph.push_str(s);
-                            }
-                        }
-                        out_html.push_str(&format!("<p>{}</p>\n", paragraph));
+                        out_html.push_str(&format!("<p>{}</p>\n", generate_html_for_text(childs)));
                     }
                     OrgElement::Section(_) => {
                         out_html
