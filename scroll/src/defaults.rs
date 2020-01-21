8f407ff5575e1ae7a4d98378b27671cdac762817
metin ur <metinur.1311@gmail.com>
2020-01-21 18:31:23 +0300

    added meta tag

--- scroll/src/defaults.rs
+++ scroll/src/defaults.rs
@@ -7,6 +7,7 @@ lazy_static! {
 <html>
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/theme.css">
     <link rel="stylesheet" type="text/css" href="/scroll_style.css">
   </head>
